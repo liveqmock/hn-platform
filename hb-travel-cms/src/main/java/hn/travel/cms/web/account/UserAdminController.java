31ba1755dc8a146130883351220bc30a19ef8263
zchen01 <745317762@qq.com>
2014-08-24 18:17:25 +0800

    cc

--- hb-travel-cms/src/main/java/hn/travel/cms/web/account/UserAdminController.java
+++ hb-travel-cms/src/main/java/hn/travel/cms/web/account/UserAdminController.java
@@ -9,13 +9,23 @@ import hn.travel.cms.generic.web.GenericController;
 import hn.travel.persist.entity.User;
 import hn.travel.persist.service.account.AccountService;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.ecside.util.RequestUtil;
+import org.extremecomponents.table.limit.Limit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,12 +45,51 @@ public class UserAdminController extends GenericController {
 	@Autowired
 	private AccountService accountService;
 
-	@RequestMapping()
+	
 	public String list(User user, Model model) {
 		model.addAttribute("user", user);
 		List<User> users = accountService.getAllUser(user);
 		model.addAttribute("users", users);
 		return "account/list";
+	}
+	
+	@RequestMapping()
+	public String serverList(User user, HttpServletRequest request, Model model) {
+		/*Date d = new Date();
+		for(int i=2; i<20; i++) {
+			User u = new User();
+			u.setLoginName("user" + i);
+			u.setRoles("user");
+			u.setPassword("111111");
+			u.setPlainPassword("111111");
+			u.setRegisterDate(d);
+			u.setName("username" + i);
+			accountService.registerUser(u);
+		}*/
+		
+		model.addAttribute("user", user);
+		int pageSize = getParam(request, "ec_rd", 13);
+		int pageNo = getParam(request, "ec_p", 1);
+		/*获取总记录数*/
+		int totalRows = (int)accountService.count(user);
+		String tableId = RequestUtil.getTableId(request);
+		Limit limit = RequestUtil.getLimit(request, tableId, totalRows, pageSize);
+		limit.setRowAttributes(totalRows, pageSize);
+		org.extremecomponents.table.limit.Sort sort = limit.getSort();
+		String sortKey = sort.getProperty();
+		String sortValue = sort.getSortOrder();
+		Pageable pageable = null;
+		if(StringUtils.hasText(sortKey)) {
+			if("desc".equals(sortValue)) 
+				pageable = new PageRequest((pageNo-1), pageSize, new Sort(Direction.DESC, sortKey));
+			else 
+				pageable = new PageRequest((pageNo-1), pageSize, new Sort(Direction.ASC, sortKey));
+		} else {
+			pageable = new PageRequest((pageNo-1), pageSize, new Sort(Direction.DESC, "registerDate"));
+		}
+		Page<User> page = accountService.findAll(user, pageable);
+		model.addAttribute("users", page.getContent());
+		return "account/serList";
 	}
 
 	@RequestMapping(value = "update/{id}", method = RequestMethod.GET)
